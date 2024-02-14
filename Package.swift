0789ac2b723a395a63c8f223f3374fb2792dd95e
BorisKly <bklykavka@gmail.com>
2024-02-14 21:55:16 +0200

    add package

--- Package.swift
+++ Package.swift
@@ -5,19 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "MyPackage",
+    platforms: [.iOS(.v15),
+        .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyPackage",
             targets: ["MyPackage"]),
     ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyPackage"),
-        .testTarget(
-            name: "MyPackageTests",
-            dependencies: ["MyPackage"]),
+            name: "MyPackage")
     ]
 )
